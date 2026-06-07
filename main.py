da63e2d50db89f1348aa94b14b52b2386b458fb0
Carlos Ledezma <carlo.s2530@outlook.com>
2026-06-07 15:53:28 -0500

    Version 1.0: Bloc de notas basico con area de texto

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ window.title("CNote")
 #height and width of the window
 window.geometry("400x300")
 
+#Text area
+text_area = tk.Text(window, font=("Arial", 12))
+text_area.pack(expand=True, fill="both", padx=10, pady=10)
+
 #bucle
 window.mainloop()
